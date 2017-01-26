6ea3c57febb6b0fc4db3b769574ff8d149a13feb
muyanbiao <muyanbiao@gmail.com>
2017-01-26 11:29:56 +0800

    按类别来区分学生基础类

---
+++ chapter5/sample5-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+bool fgrade(const Student_info& s)
+{
+	return grade(s) < 60;
+}
+
+vector<Student_info> extract_fails(vector<Student_info>& students)
+{
+	
+}
